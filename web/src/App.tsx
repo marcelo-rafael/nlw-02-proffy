e3a3bd090b3c98b15a4845741f0dda0c1b34f836
marcelo-rafael <marcelo.rafael.goncalves@gmail.com>
2020-10-08 08:41:18 -0300

    criando projeto/criando estilos globais

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './assets/styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Marcelo</h1>
+    </>
+  );
+}
+
+export default App;
